3106387922f16ab748d181ae60727154cdb48fa2
Alexander Moore <truckfondue@gmail.com>
2015-04-14 08:13:10 -0600

    week3 initial commit

---
+++ loopPractice2/main.js
@@ -0,0 +1,15 @@
+$(document).on('ready', function() {
+
+
+// booiler plate
+	// var animals = ['rat', 'cat', 'butterfly', 'marmot', 'ocelot'];
+	// for(var i=0; i<animals.length; i++) {
+ // 		console.log(animals[i]);
+	// };
+
+	var animals = ['rat', 'cat', 'butterfly', 'marmot', 'ocelot'];
+	for(var i=0; i<(animals.length - 1); i++) {
+ 		console.log(animals[i]);
+	};
+  
+});
