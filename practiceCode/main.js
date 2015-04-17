5f0dd2afb58a147f51d3b6bcada0be41f9d3c168
Alex Moore <truckfondue@gmail.com>
2015-04-17 13:56:24 -0600

    starCraftRankings finished via an API

---
+++ practiceCode/main.js
@@ -0,0 +1,12 @@
+var x = 'Brad Pitt';
+//  Charlie Sheen
+var results = x === 'Charlie Sheen'  ?  'WINNING!' : 'Tiger Blood!';
+console.log(results);
+
+
+
+function stringReverse(str) {
+	return str.reverse();
+}
+
+console.log(stringReverse('amazon'));
